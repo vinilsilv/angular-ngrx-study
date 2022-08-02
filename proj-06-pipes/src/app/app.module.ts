88a4f6530577c43162875dff44610995ccacd4c6
Vincius Silva <vinilrsilv@gmail.com>
2022-08-02 18:45:07 -0300

    [06 - Pipes]: Pipes examples added

--- proj-06-pipes/src/app/app.module.ts
+++ proj-06-pipes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesExampleComponent } from './pipes-example/pipes-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesExampleComponent
   ],
   imports: [
     BrowserModule,
---
+++ proj-06-pipes/src/app/pipes-example/pipes-example.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes-example',
+  templateUrl: './pipes-example.component.html',
+  styleUrls: ['./pipes-example.component.scss']
+})
+export class PipesExampleComponent implements OnInit {
+
+  book: any = {
+    title: 'Title of a book',
+    rating: 4.56521358,
+    price: 10.45,
+    releaseDate: new Date(2016, 5, 20)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
