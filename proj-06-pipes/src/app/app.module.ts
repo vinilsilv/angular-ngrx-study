e1951850631e3f714e55af0a244205f0d85eec8f
Vincius Silva <vinilrsilv@gmail.com>
2022-08-02 18:48:10 -0300

    [06 - Pipes]: Custom pipe created

--- proj-06-pipes/src/app/app.module.ts
+++ proj-06-pipes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PipesExampleComponent } from './pipes-example/pipes-example.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PipesExampleComponent
+    PipesExampleComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule,
