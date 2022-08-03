2a546c39196bdbfb5ef914a30423d87733ef8d47
Vincius Silva <vinilrsilv@gmail.com>
2022-08-03 09:15:11 -0300

    [Requests HTTP]: Environment variables with API url

--- requests-http/src/app/cursos.service.ts
+++ requests-http/src/app/cursos.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Curso } from './cursos/curso';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CursosService {
-  private readonly apiUrl = 'http://localhost:3000/cursos';
+  private readonly apiUrl = `${environment.API_URL}/cursos`;
 
   constructor(private http: HttpClient) {}
 
