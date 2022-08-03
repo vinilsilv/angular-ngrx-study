2eea2a43f9ce4ad30d8e6adbe9daa558cad36383
Vincius Silva <vinilrsilv@gmail.com>
2022-08-02 22:16:23 -0300

    [Requests HTTP]: Get data from fake server

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from 'src/app/cursos.service';
+import { Curso } from '../curso';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true,
 })
 export class CursosListaComponent implements OnInit {
+  cursos!: Curso[];
 
-  constructor() { }
+  constructor(private cursosService: CursosService) {}
 
   ngOnInit(): void {
+    this.cursosService.list().subscribe({
+      next: (res) => {
+        this.cursos = res,
+        console.log(this.cursos)
+      },
+    });
   }
-
 }
