09e1bcb4ff57f7e32cc3ce6ba84de076e4df41c6
Vincius Silva <vinilrsilv@gmail.com>
2022-08-19 14:28:09 -0300

    [Routes]: Environment fix + new components

--- routes/src/app/app.module.ts
+++ routes/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
