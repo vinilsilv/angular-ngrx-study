94ea1ce4b40d4fbdb742b11021db447ace28b13c
Vincius Silva <vinilrsilv@gmail.com>
2022-08-22 19:37:14 -0300

    [Routes]: Route parameters

--- routes/src/app/app.routing.ts
+++ routes/src/app/app.routing.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
 import { CursosComponent } from "./cursos/cursos.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
@@ -7,6 +8,7 @@ import { LoginComponent } from "./login/login.component";
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent }
 ]
 
