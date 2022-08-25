6be56d5a918906599f014b7a77fe9945e7d69725
Vincius Silva <vinilrsilv@gmail.com>
2022-08-25 13:39:16 -0300

    [Routes]: Query params

--- routes/src/app/cursos/cursos.component.ts
+++ routes/src/app/cursos/cursos.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  styleUrls: ['./cursos.component.scss'],
 })
-export class CursosComponent implements OnInit {
-
+export class CursosComponent implements OnInit, OnDestroy {
   cursos!: any[];
+  pagina!: number;
+  inscricao!: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe({
+      next: (queryParams) => {
+        this.pagina = queryParams['pagina'];
+
+      },
+    });
   }
 
+  ngOnDestroy(){
+    this.inscricao.unsubscribe()
+  }
+
+  paginaSeguinte() {
+    this.router.navigate(['/cursos'], 
+    {queryParams: {'pagina': ++this.pagina}});
+  }
+
+  paginaAnterior() {
+    this.router.navigate(['/cursos'], 
+    {queryParams: {'pagina': --this.pagina}});
+  }
 }
