befe91b3387225d5d0cd47fb6040ad52298e88e4
Vincius Silva <vinilrsilv@gmail.com>
2022-08-25 21:37:36 -0300

    [Routes]: Using routing module

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import { CursosComponent } from "./cursos/cursos.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'nao-encontrado', component: CursoNaoEncontradoComponent }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- routes/src/app/app.module.ts
+++ routes/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { MaterializeModule } from 'angular2-materialize';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
+// import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -24,8 +24,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterializeModule,
-    routing
+    MaterializeModule
+    // routing
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
