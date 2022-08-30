a60ce2be3d33607eb459b63151808222938f7856
Vincius Silva <vinilrsilv@gmail.com>
2022-08-30 15:13:56 -0300

    [Forms]: Project setup

---
+++ forms/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
+const routes: Routes = [
+  { path: 'template-form', component: TemplateFormComponent },
+  { path: 'data-form', component: DataFormComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'template-form' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
