d31595fd5c60275a1bb3c46eb5270a225a7c7e19
Vincius Silva <vinilrsilv@gmail.com>
2022-09-01 08:42:28 -0300

    [Forms]: Applying style based on validation

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Vinicius',
-    email: 'vinicius@email.com'
+    nome: null,
+    email: null
   }
 
   constructor() { }
