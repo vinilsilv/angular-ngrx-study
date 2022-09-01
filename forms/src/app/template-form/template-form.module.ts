db0bb36388cd7d6a1fddf4adaf00ad5b44c42d5c
Vincius Silva <vinilrsilv@gmail.com>
2022-09-01 11:09:32 -0300

    [Forms]: Adding component in order to easily debug form values

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent],
+  declarations: [TemplateFormComponent, FormDebugComponent],
   imports: [CommonModule, FormsModule],
 })
 export class TemplateFormModule {}
