7bfd2c21609e7d558a64c4f02f7d49984cf6f790
Vincius Silva <vinilrsilv@gmail.com>
2022-09-01 12:24:05 -0300

    [Forms]: Refactoring

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 
 @NgModule({
-  declarations: [TemplateFormComponent, FormDebugComponent],
+  declarations: [
+    TemplateFormComponent,
+    FormDebugComponent,
+    CampoControlErroComponent,
+  ],
   imports: [CommonModule, FormsModule],
 })
 export class TemplateFormModule {}
