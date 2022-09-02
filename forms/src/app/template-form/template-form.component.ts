f12cf2f5aeb08dc609185fa64e8f79e27713cd61
Vincius Silva <vinilrsilv@gmail.com>
2022-09-02 17:31:33 -0300

    [Forms]: Automatically retrieving data from a zip code

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,37 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.scss']
+  styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
-
   usuario: any = {
     nome: null,
-    email: null
-  }
+    email: null,
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(form: any) {
-    console.log(form.form)
+    console.log(form.form);
 
-    console.log(this.usuario)
+    console.log(this.usuario);
   }
 
-  verificaValidTouched(campo: any){
-    return !campo.valid && campo.touched
+  verificaValidTouched(campo: any) {
+    return !campo.valid && campo.touched;
   }
 
-  aplicaCssErro(campo: any){
+  aplicaCssErro(campo: any) {
     return {
       'has-error': this.verificaValidTouched(campo),
-      'has-feedback': this.verificaValidTouched(campo)
-  }
+      'has-feedback': this.verificaValidTouched(campo),
+    };
   }
 
+  consultaCEP(cep: any) {
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != '') {
+      let validaCep = /^[0-9]{8}$/;
+
+      if (validaCep.test(cep)) {
+        this.http
+          .get(`https://viacep.com.br/ws/${cep}/json/`)
+          .subscribe({ next: (dados) => console.log(dados) });
+      }
+    }
+  }
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -4,6 +4,7 @@ import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -11,6 +12,6 @@ import { CampoControlErroComponent } from '../campo-control-erro/campo-control-e
     FormDebugComponent,
     CampoControlErroComponent,
   ],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
 })
 export class TemplateFormModule {}
