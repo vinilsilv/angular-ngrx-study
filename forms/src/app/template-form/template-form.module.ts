6b89667a46c973269de771fdc6c5c77881e1040e
Vincius Silva <vinilrsilv@gmail.com>
2022-09-07 18:39:33 -0300

    [Forms]: Synchronizing HTML with FormGroup

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
-import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    TemplateFormComponent,
-    FormDebugComponent,
-    CampoControlErroComponent,
-  ],
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  declarations: [TemplateFormComponent],
+  imports: [CommonModule, FormsModule, HttpClientModule, SharedModule],
 })
 export class TemplateFormModule {}
