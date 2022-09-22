2bf5f1094d44c2cc7caad0a753c3e28888b68780
Vinicius Silva <vinilrsilv@gmail.com>
2022-09-22 09:25:23 -0300

    [Forms]: Custom input field (ControlValueAccessor)

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -4,11 +4,23 @@ import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
 import { DropdownService } from './services/dropdown.service';
 import { ErrorMsgComponent } from './error-msg/error-msg.component';
+import { InputFieldComponent } from './input-field/input-field.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [FormDebugComponent, CampoControlErroComponent, ErrorMsgComponent],
-  imports: [CommonModule],
-  exports: [FormDebugComponent, CampoControlErroComponent, ErrorMsgComponent],
-  providers: [DropdownService]
+  declarations: [
+    FormDebugComponent,
+    CampoControlErroComponent,
+    ErrorMsgComponent,
+    InputFieldComponent,
+  ],
+  imports: [CommonModule, FormsModule],
+  exports: [
+    FormDebugComponent,
+    CampoControlErroComponent,
+    ErrorMsgComponent,
+    InputFieldComponent,
+  ],
+  providers: [DropdownService],
 })
 export class SharedModule {}
